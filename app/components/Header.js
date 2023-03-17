7a58c717ea57a899904fa17d46f3b2be184fb5d1
blessedmadukoma <blessedmadukoma@gmail.com>
2023-03-17 09:59:44 +0100

    updated class to className

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,26 +1,26 @@
 const Header = () => {
   return (
-    <header class="flex justify-end">
-      <section class="flex items-center justify-items-end space-x-3">
-        <div class="flex h-8 items-center overflow-hidden rounded-full bg-white">
+    <header className="flex justify-end">
+      <section className="flex items-center justify-items-end space-x-3">
+        <div className="flex h-8 items-center overflow-hidden rounded-full bg-white">
           <input
             type="text"
-            class=" h-8 rounded-full p-2  placeholder:items-center placeholder:text-xs"
+            className=" h-8 rounded-full p-2  placeholder:items-center placeholder:text-xs"
             placeholder="Search Transaction"
           />
 
           <img
-            class="mr-3 h-5 w-5 items-center"
+            className="mr-3 h-5 w-5 items-center"
             src="assets/search.svg"
             alt=""
           />
         </div>
         <div>
-          <img src="assets/notification.svg" class="h-5 w-5" alt="" />
+          <img src="assets/notification.svg" className="h-5 w-5" alt="" />
         </div>
-        <span class="text-gray-500">|</span>
+        <span className="text-gray-500">|</span>
         <div>
-          <img src="assets/avatar.png" class="h-8 w-8" alt="" />
+          <img src="assets/avatar.png" className="h-8 w-8" alt="" />
         </div>
       </section>
     </header>
